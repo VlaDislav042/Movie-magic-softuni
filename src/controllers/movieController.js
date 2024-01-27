372123b1826bc3e1ac107d85b33e40ddb19424b4
Vladislav Georgiev <vlafi90@gmail.com>
2024-01-27 16:23:50 +0200

    add dynamic detail page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -8,7 +8,14 @@ router.get('/create', (req, res) => {
 router.post('/create', (req, res) => {
     const newMovie = req.body;
     movieService.create(newMovie);
-    res.render('home')
+    res.redirect('/')
+});
+
+router.get('/movies/:movieId', (req, res) => {
+    const movieId = req.params.movieId;
+    const movie = movieService.getOne(movieId);
+
+    res.render('details', {movie});
 });
 
 module.exports = router;
