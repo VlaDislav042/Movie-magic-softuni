356c947c6e072986fd33e936f0ff277638c6c3b3
Vladislav Georgiev <vlafi90@gmail.com>
2024-01-27 14:14:05 +0200

    add a movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,5 +1,14 @@
-const movies = [];
+const movies = [{
+    title: 'FF9',
+    genre: 'Action',
+    director: 'Ivan Ivanov',
+    date: '2022',
+    imageUrl: '/img/the-little-mermaid.jpg',
+    rating: '5',
+    description: 'fdsfsdfsd'
+  }];
 
 exports.create = (movieData) => {
+    console.log(movieData);
     movies.push(movieData);
 };
