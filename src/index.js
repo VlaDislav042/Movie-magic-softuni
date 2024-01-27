f20be1a58c0da45668a4a91012fb9ffc694748dc
Vladislav Georgiev <vlafi90@gmail.com>
2024-01-27 11:49:52 +0200

    add resources and render home page

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const path = require('path');
 const app = express();
-const port = 5000
+const port = 5000;
 
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
-
 }));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'))
 
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('home', {layout: false});
