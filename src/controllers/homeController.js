aea7fb87ead0db2e959f1696e054e9f762e8bfbc
Vladislav Georgiev <vlafi90@gmail.com>
2024-01-27 13:28:18 +0200

    add error page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,7 +3,13 @@ const router = require("express").Router();
 router.get('/', (req, res) => {
     res.render('home');
 });
- router.get('/about', (req, res) => {
+
+router.get('/about', (req, res) => {
     res.render('about');
- })
+})
+
+router.get('*', (req, res) => {
+    res.render('404')
+})
+
 module.exports = router;
