d55cc0f5f86a0718cacc994d558f766bf16620f2
Vladislav Georgiev <vlafi90@gmail.com>
2024-01-30 12:41:21 +0200

    update details page and cast members

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -20,6 +20,7 @@ router.post('/create', async(req, res) => {
 router.get('/movies/:movieId', async(req, res) => {
     const movieId = req.params.movieId;
     const movie = await movieService.getOne(movieId).lean();
+    
 
     res.render('details', {movie});
 });
@@ -28,6 +29,12 @@ router.get('/movies/:movieId/attach', async(req, res) => {
     const movie = await movieService.getOne(req.params.movieId).lean();
     const casts = await castService.getAll().lean();
     res.render('movie/attach', { ...movie, casts });
+})
+
+router.post('/movies/:movieId/attach', async(req, res) => {
+    const castId = req.body.cast;
+    await movieService.attach(req.params.movieId, castId);
+    res.redirect(`/movies/${req.params.movieId}/attach`);
 })
 
 module.exports = router;
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,18 +1,20 @@
 const Movie = require('../models/Movie');
 
 
-
 exports.getAll = () => {
     const movies =  Movie.find();
     return movies;
-}
+};
 
-exports.getOne = (movieId) => {
-    const movie = Movie.findById(movieId);
-    return movie;
-}
+exports.getOne = (movieId) => Movie.findById(movieId).populate('casts');
 
 exports.create = (movieData) => {
     return Movie.create(movieData);
     
 };
+
+exports.attach = async(movieId, castId) => {
+   const movie = await this.getOne(movieId);
+   movie.casts.push(castId);
+   return movie.save();
+}
