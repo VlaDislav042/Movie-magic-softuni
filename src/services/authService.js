d3f9ea1dc66e1998a5699aa0415a2e51f2388b1a
Vladislav Georgiev <vlafi90@gmail.com>
2024-01-31 20:47:57 +0200

    add token for user

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,3 +1,25 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('../lib/jwt');
+const SECRET = 'asfsadgfsdgsaga';
 
 exports.register = (userData) => User.create(userData);
+
+exports.login = async (email, password) => {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error('Cannot find username ot password');
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+        throw new Error('Cannot find username ot password');
+    }
+    const payload = {
+        _id: user._id,
+        email: user.email,
+    } 
+    const token = await jwt.sign(payload, SECRET, { expiresIn: '2h'});
+    return token;
+}
