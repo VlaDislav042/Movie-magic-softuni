a248a8d39fd2498e9b049b54885f7ff78880b726
Vladislav Georgiev <vlafi90@gmail.com>
2024-01-31 18:27:02 +0200

    add register page

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,12 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const movieController = require('./controllers/movieController');
 const castController = require('./controllers/castController');
+const authController = require('./controllers/authController');
 
+router.use('/auth', authController);
 router.use('/cast', castController);
 router.use(movieController);
 router.use(homeController);
+
 
 module.exports = router;
